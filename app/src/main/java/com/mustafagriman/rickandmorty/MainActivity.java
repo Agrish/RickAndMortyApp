02330574bc79658e440b710c9d5c53efb830c5a6
Agrish <mustafagriman94@hotmail.com>
2022-04-25 01:01:48 +0300

    Initial commit

---
+++ app/src/main/java/com/mustafagriman/rickandmorty/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mustafagriman.rickandmorty;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
